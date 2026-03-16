bf2654aa1352c63b438527ee1708adf576d78fab
utkucmn11-max <utkucmn11@gmail.com>
2026-03-16 16:35:22 +0300

    Update web_sitem.py

--- web_sitem.py
+++ web_sitem.py
@@ -207,7 +207,7 @@ with t4:
 # --- PROJELER ---
 st.divider()
 st.header( "💻 Projelerim" )
-with st.expander(" Devam Eden Çalışmalar",expanded=True):
+with st.expander(" Devam eden çalışmalar ",expanded=True):
     st.write("Python tabanlı otomasyon sistemleri üzerine odaklanıyorum.")
 
 st.write("### 🎮 Hobiler")
